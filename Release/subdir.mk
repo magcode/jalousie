e0ecf7f5a282200c33c02eca79409962625bfabb
magcode <marko.web1@outlook.com>
2017-03-26 16:47:03 +0200

    ignore files

--- Release/subdir.mk
+++ Release/subdir.mk
@@ -8,18 +8,21 @@ INO_SRCS += \
 
 CPP_SRCS += \
 ..\.ino.cpp \
-..\J_down.cpp 
+..\J_down.cpp \
+..\J_up.cpp 
 
 LINK_OBJ += \
 .\.ino.cpp.o \
-.\J_down.cpp.o 
+.\J_down.cpp.o \
+.\J_up.cpp.o 
 
 INO_DEPS += \
 .\jalousie.ino.d 
 
 CPP_DEPS += \
 .\.ino.cpp.d \
-.\J_down.cpp.d 
+.\J_down.cpp.d \
+.\J_up.cpp.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -31,6 +34,13 @@ CPP_DEPS += \
 	@echo ' '
 
 J_down.cpp.o: ../J_down.cpp
+	@echo 'Building file: $<'
+	@echo 'Starting C++ compile'
+	"C:\Users\Marko\privat\ha\sloeber\arduinoPlugin\tools\arduino\avr-gcc\4.9.2-atmel3.5.3-arduino2/bin/avr-g++" -c -g -Os -std=gnu++11 -fpermissive -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -flto -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10609 -DARDUINO_AVR_NANO -DARDUINO_ARCH_AVR   -I"C:\Users\Marko\privat\ha\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.16\cores\arduino" -I"C:\Users\Marko\privat\ha\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.16\variants\eightanaloginputs" -I"C:\Users\Marko\privat\ha\sloeber\arduinoPlugin\libraries\Automaton\1.0.1" -I"C:\Users\Marko\privat\ha\sloeber\arduinoPlugin\libraries\Automaton\1.0.1\src" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -D__IN_ECLIPSE__=1 -x c++ "$<" -o "$@"  -Wall
+	@echo 'Finished building: $<'
+	@echo ' '
+
+J_up.cpp.o: ../J_up.cpp
 	@echo 'Building file: $<'
 	@echo 'Starting C++ compile'
 	"C:\Users\Marko\privat\ha\sloeber\arduinoPlugin\tools\arduino\avr-gcc\4.9.2-atmel3.5.3-arduino2/bin/avr-g++" -c -g -Os -std=gnu++11 -fpermissive -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -flto -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10609 -DARDUINO_AVR_NANO -DARDUINO_ARCH_AVR   -I"C:\Users\Marko\privat\ha\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.16\cores\arduino" -I"C:\Users\Marko\privat\ha\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.16\variants\eightanaloginputs" -I"C:\Users\Marko\privat\ha\sloeber\arduinoPlugin\libraries\Automaton\1.0.1" -I"C:\Users\Marko\privat\ha\sloeber\arduinoPlugin\libraries\Automaton\1.0.1\src" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -D__IN_ECLIPSE__=1 -x c++ "$<" -o "$@"  -Wall
